24aa459b6f329f0cbfa8c7f585f1d79bd554ee39
fredrik <freedrik92@hotmail.com>
2025-06-03 13:32:29 +0200

    added connection string

--- BookingApi/Program.cs
+++ BookingApi/Program.cs
@@ -1,12 +1,26 @@
 using BookingApi.Data;
 using BookingApi.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<BookingDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
+
 builder.Services.AddScoped<BookingRepos>();
 builder.Services.AddScoped<BookingServvie>();
+builder.Services.AddCors(x =>
+{
+    x.AddPolicy("AllowAll", x =>
+    {
+        x.AllowAnyOrigin();
+        x.AllowAnyHeader();
+        x.AllowAnyMethod();
+    });
+
+});
 var app = builder.Build();
 
 
@@ -14,6 +28,7 @@ var app = builder.Build();
 
 app.MapOpenApi();
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
